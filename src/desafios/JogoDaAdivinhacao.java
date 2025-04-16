dbfc2504949dd01d5e8a6fa18c1cb6fb1b0ca3c8
Antônio de Pádua <antonyejoao@gmail.com>
2025-04-16 16:36:10 -0300

    implementando jogo da adivinhação

---
+++ src/desafios/JogoDaAdivinhacao.java
@@ -0,0 +1,32 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class JogoDaAdivinhacao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int resposta = 4;
+        int chute = 0;
+        int tentativas = 0;
+
+        do {
+            System.out.println("Digite um número de 1 a 100\n\n");
+            chute = sc.nextInt();
+
+            if (chute == resposta) {
+                System.out.println("Parabéns! Você acertou! com " + tentativas + " tentativas!");
+            } else {
+                System.out.println("errou... tente novamente!");
+            }
+
+            if (chute < resposta) {
+                System.out.println("O número é maior que " + resposta);
+            } else if (chute > resposta) {
+                System.out.println("O número é menor que " + resposta);
+            }
+
+            tentativas = tentativas + 1;
+
+        } while (chute != resposta);
+    }
+}
